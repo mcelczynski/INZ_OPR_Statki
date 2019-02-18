85566c913afd9c23ee7ba8df4759951f5f4b1ea5
mcelczynski <Celczynski1>
2019-02-18 23:49:25 +0100

    Version 0.44
    Added arrays sorting

--- src/Rozlokuj.java
+++ src/Rozlokuj.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Rozlokuj {
 
@@ -12,13 +14,31 @@ public class Rozlokuj {
         int ilosc_lini = Funkcje.ilosc_lini(plik_kontenery)-1;
         System.out.println("Rozpoczynam rozlokowywanie " + ilosc_lini + " kontenerow na statki z bazy " + baza_statki);
         String [][] kontenery = Funkcje.czytaj_z_csv(plik_kontenery);
+        System.out.println(Arrays.deepToString(kontenery));
+        Arrays.sort(kontenery, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] a, String[] b) {
+                return Double.compare(Double.parseDouble(a[4]), Double.parseDouble(b[4]));
+            }
+        });
 
-        for(int x=0; x< kontenery.length; x++){
-            for(int y=0; y< kontenery[x].length; y++)
+        System.out.println("Posortowana tabela: \n" + Arrays.deepToString(kontenery));
+        String [][] tmp = new String [kontenery.length+3][(kontenery.length+3)];
+        tmp[0][5]="1";
+        System.out.println("Tymczasowa tabela: \n" + Arrays.deepToString(tmp));
+
+        System.arraycopy(kontenery,0,tmp,0,(kontenery.length));
+              // tmp = Arrays.copyOf(kontenery, kontenery.length);
+        tmp[0][5]="1";
+        System.out.println("Rozszerzona tabela: \n" + Arrays.deepToString(tmp));
+        System.out.println("Tymczasowa tabela recznie: \n" + tmp[0][0] + tmp[0][4]);
+
+
+        /*    for(int y=0; y< kontenery[x].length; y++)
                 System.out.print(kontenery[x][y] + "    ");
             System.out.println();
         }
-
+*/
 
     return ("Skonczylem rozlokowanie");
     }
